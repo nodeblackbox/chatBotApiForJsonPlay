2bc64e2749be60b6d80ae6a55d1dd5e233e688c1
NASA <65771915+nodeblackbox@users.noreply.github.com>
2023-04-07 18:28:37 +0100

    New update everything working well It serves the json data
    Speedrun tested complete and everything
    Looks on ports 3000 http://localhost:3000/api/chatbot/responses

--- app.js
+++ app.js
@@ -7,3 +7,18 @@ app.use(express.json());
 app.listen(port, () => {
   console.log(`Chatbot API listening at http://localhost:${port}`);
 });
+
+const fs = require("fs");
+const path = require("path");
+
+app.get("/api/chatbot/responses", (req, res) => {
+  const dataPath = path.join(__dirname, "data", "responses.json");
+
+  fs.readFile(dataPath, "utf8", (err, data) => {
+    if (err) {
+      res.status(500).json({ message: "Error reading JSON data" });
+    } else {
+      res.json(JSON.parse(data));
+    }
+  });
+});
