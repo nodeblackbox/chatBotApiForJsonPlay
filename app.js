c199ab17e1eb14e39a9aee164a53aa8e962fa12b
NASA <65771915+nodeblackbox@users.noreply.github.com>
2023-04-07 18:46:03 +0100

    full working semi full stack
    Tests work posted API are successful
    
    {"greetings":["Hello! How can I help you?","Hi there! What can I do for you today?"],"farewells":["Goodbye! Have a great day!","Bye! Take care!"],"cool cars":["omg i like them too have you seen one of my cars"]}
    
    Add new categories
    
    I need to do state testing testing Just responses and just categories
    
    Also I want to implement an API key system

--- app.js
+++ app.js
@@ -3,14 +3,15 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
-
-app.listen(port, () => {
-  console.log(`Chatbot API listening at http://localhost:${port}`);
-});
-
+//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 const fs = require("fs");
 const path = require("path");
 
+//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 app.get("/api/chatbot/responses", (req, res) => {
   const dataPath = path.join(__dirname, "data", "responses.json");
 
@@ -21,4 +22,57 @@ app.get("/api/chatbot/responses", (req, res) => {
       res.json(JSON.parse(data));
     }
   });
+});
+//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+function updateJsonFile(filePath, updateFunction, callback) {
+  fs.readFile(filePath, "utf8", (err, data) => {
+    if (err) {
+      callback(err);
+    } else {
+      let jsonData = JSON.parse(data);
+      jsonData = updateFunction(jsonData);
+      fs.writeFile(
+        filePath,
+        JSON.stringify(jsonData, null, 2),
+        "utf8",
+        (err) => {
+          callback(err);
+        }
+      );
+    }
+  });
+}
+//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+app.post("/api/chatbot/responses", (req, res) => {
+  const dataPath = path.join(__dirname, "data", "responses.json");
+  const { category, response } = req.body;
+
+  if (!category || !response) {
+    res
+      .status(400)
+      .json({ message: "Please provide a category and a response" });
+    return;
+  }
+
+  updateJsonFile(
+    dataPath,
+    (jsonData) => {
+      if (!jsonData[category]) {
+        jsonData[category] = [];
+      }
+      jsonData[category].push(response);
+      return jsonData;
+    },
+    (err) => {
+      if (err) {
+        res.status(500).json({ message: "Error updating JSON data" });
+      } else {
+        res.status(201).json({ message: "Response added successfully" });
+      }
+    }
+  );
+});
+//@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+app.listen(port, () => {
+  console.log(`Chatbot API listening at http://localhost:${port}`);
 });
