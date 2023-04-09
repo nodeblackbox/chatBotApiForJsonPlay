d0e1e364c1e5963dbd16fa8d96148035b84e9bb9
NASA <65771915+nodeblackbox@users.noreply.github.com>
2023-04-09 23:58:07 +0100

    adding the Canvas behind All of the HTML elements
    Todo: I would next start designing the UI And building it in CSS and HTML  the UI should have should be a chat box that  allows you to post messages And the messages will stack on top of each other

--- sketch.js
+++ sketch.js
@@ -1,18 +1,41 @@
-function setup() {
-  createCanvas(windowWidth, windowHeight);
-  // Start AudioContext in response to user gesture
-  document.addEventListener("click", function () {
-    getAudioContext().resume();
-  });
-  // canvas.parent("jimjam");
-}
-
-// function windowResized() {
-//   resizeCanvas(windowWidth, windowHeight);
+// function setup() {
+//   let canvas = createCanvas(windowWidth, windowHeight);
+//   canvas.id("myCanvas"); // Assign an ID to the canvas for CSS targeting
+//   // canvas.parent("jimjam");
 // }
 
+// function draw() {
+//   background(220);
+//   background(220);
+//   fill(255, 0, 0);
+//   rect(
+//     windowWidth - windowWidth + 100,
+//     windowHeight - windowHeight + 100,
+//     100,
+//     100
+//   );
+// }
+
+// function windowResized() {
+//   resizeCanvas(windowWidth, windowHeight); // Resize the canvas when the window is resized
+// }
+
+function setup() {
+  let canvas = createCanvas(windowWidth, windowHeight);
+  canvas.id("myCanvas"); // Assign an ID to the canvas for CSS targeting
+}
+
 function draw() {
-  background(220);
+  background(0, 20, 20);
   fill(255, 0, 0);
-  rect(100, 100, 100, 100);
+  rect(
+    windowWidth - windowWidth + 100,
+    windowHeight - windowHeight + 100,
+    100,
+    100
+  );
+}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight); // Resize the canvas when the window is resized
 }
